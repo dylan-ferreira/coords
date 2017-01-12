c92b7e6ae837ee76feec794afb20b8eace1369ca
Dylan Ferreira <dferreira@basepair.org>
2017-01-12 15:11:26 -0800

    Add files via upload

---
+++ ex_coords.js
@@ -0,0 +1,26 @@
+getStuff([
+  {
+    "key": "sprocket",
+    "latitude": 32.784235, 
+    "longitude": -96.794522,
+    "name": "Sprocket"
+  },
+  {
+    "key": "chd1",
+    "latitude": 33.275057,
+    "longitude": -111.886905,
+    "name": "DC14"
+  },
+  {
+    "key": "phl1",
+    "latitude": 39.860664,
+    "longitude": -75.299303,
+    "name": "DC18"
+  },
+  {
+    "key": "atl1",
+    "latitude": 33.746222,
+    "longitude": -84.393360,
+    "name": "DC8"
+  }
+])
